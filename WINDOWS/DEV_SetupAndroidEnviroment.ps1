5e45965c5ccdbb372698a8f3c6c67cdc2a7978e9
Rafael Gomez <ravigomez@gmail.com>
2019-10-04 00:45:16 -0300

    criando arquivos

---
+++ WINDOWS/DEV_SetupAndroidEnviroment.ps1
@@ -0,0 +1,9 @@
+
+#COMENTÁRIOS
+#DO CÓDIGO 
+Write-Host "START ->>>>>> Setup Android develpment enviroment"
+
+choco install -y nodejs.install python2 jdk8
+
+
+Write-Host "END SETUP"
---
+++ WINDOWS/_SETUP_CleanWindows.ps1
@@ -0,0 +1,10 @@
+
+Write-Host "BEGINING...."
+
+& @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+Set-ExecutionPolicy AllSigned
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+Write-Host "ENDED...."
