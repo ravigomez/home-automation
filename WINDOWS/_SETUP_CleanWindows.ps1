a4702898b194dc29f55028665f9977a684da6a27
ravigomez <ravigomez@gmail.com>
2019-10-09 00:28:02 -0300

    Teste

--- WINDOWS/_SETUP_CleanWindows.ps1
+++ WINDOWS/_SETUP_CleanWindows.ps1
@@ -1,6 +1,10 @@
 
 Write-Host "BEGINING...."
 
+Write-Host "TEste"
+
+
+
 & @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 Set-ExecutionPolicy AllSigned
