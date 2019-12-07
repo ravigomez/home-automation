ddea8b70c025d1610b2c3424b8b0da892e6fa5b4
Rafael Gomez <ravigomez@gmail.com>
2019-12-07 01:42:48 -0300

    primeiros scripts macos

---
+++ MAC_OS/setup_init.sh
@@ -0,0 +1,22 @@
+
+
+## Install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install htop
+
+## Install node
+brew install node
+
+## Install yarn
+brew install yarn
+
+## Visual studio code
+brew cask install visual-studio-code
+
+#install docker
+
+brew install bash-completion
+brew cask install docker
+brew install kubectl
+brew install minikube
