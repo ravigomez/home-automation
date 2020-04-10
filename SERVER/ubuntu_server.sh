4f7341f60386952b53d0839bbbde8940abf0fd70
Rafael Gomez <ravigomez@gmail.com>
2020-04-10 05:10:19 -0300

    ubuntu_server - initial commit

---
+++ SERVER/ubuntu_server.sh
@@ -0,0 +1,19 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install curl
+sudo apt-get install htop
+sudo apt-get install iotop
+sudo apt install net-tools
+
+###Live patch ubuntu
+sudo snap install canonical-livepatch
+sudo canonical-livepatch enable 79d9f22c11b44891b0b53918d176d708
+############
+
+### GIT ####
+sudo apt-get install git-core
+
+sudo git config --global user.name "ravigomez"
+sudo git config --global user.email "ravigomez@gmail.com"
+###########################
