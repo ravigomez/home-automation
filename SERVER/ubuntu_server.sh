6c8a510b61be45b0ee0f02f5b40d521fd47a659b
Rafael Gomez <ravigomez@gmail.com>
2020-04-10 19:35:04 -0300

    include docker

--- SERVER/ubuntu_server.sh
+++ SERVER/ubuntu_server.sh
@@ -1,5 +1,4 @@
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get update && sudo apt-get upgrade
 
 sudo apt-get install curl
 sudo apt-get install htop
@@ -17,3 +16,28 @@ sudo apt-get install git-core
 sudo git config --global user.name "ravigomez"
 sudo git config --global user.email "ravigomez@gmail.com"
 ###########################
+
+#####Docker 
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+#### Docker Machine
+base=https://github.com/docker/machine/releases/download/v0.16.0 &&
+  curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
+  sudo mv /tmp/docker-machine /usr/local/bin/docker-machine &&
+  chmod +x /usr/local/bin/docker-machine
