3083244d2b9974dbbf226b894a8afdf68b6630af
Rafael Gomez <ravigomez@gmail.com>
2020-04-10 19:34:56 -0300

    include docker machine

--- MAC_OS/setup_init.sh
+++ MAC_OS/setup_init.sh
@@ -52,6 +52,11 @@ brew install bash-completion
 brew cask install docker
 brew install kubectl
 brew install minikube
+
+##### Docker Machine ################
+base=https://github.com/docker/machine/releases/download/v0.16.2 &&
+  curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/usr/local/bin/docker-machine &&
+  chmod +x /usr/local/bin/docker-machine
 ###############################
 
 ### mobile configurations
