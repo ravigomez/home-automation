7e5544241a2fcc18db3faff29de6af3280d65173
Rafael Gomez <ravigomez@gmail.com>
2021-04-24 22:11:31 +0000

    Updated _SETUP_CleanWindows.ps1

--- WINDOWS/_SETUP_CleanWindows.ps1
+++ WINDOWS/_SETUP_CleanWindows.ps1
@@ -6,15 +6,10 @@ Set-ExecutionPolicy AllSigned
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 choco install googlechrome
-
 choco install openjdk ## choco install jdk8 for 32 bits
-
 choco install libreoffice-fresh #base requires java
-
 choco install 7zip.install
-
 choco install vlc
-
 choco install notepadplusplus.install
 
 choco install git
@@ -25,5 +20,8 @@ git config --global color.ui auto
 choco install nodejs.install
 
 choco install vscode
+
+choco install docker-desktop
+choco install docker-compose
 
 Write-Host "ENDED...."
